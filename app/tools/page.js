dc49d108011bf1c2f62554dbbefb1ebf469956a6
sethorpe <oluseyi.thorpe@gmail.com>
2025-03-06 17:04:49 -0800

    Add Contact

--- app/tools/page.js
+++ app/tools/page.js
@@ -3,6 +3,7 @@ import React from "react";
 import { motion } from "framer-motion";
 import dynamic from "next/dynamic";
 import Navbar from "../components/Navbar";
+import Contact from "../components/Contact";
 
 const PasswordGenerator = dynamic(() => import("../components/PasswordGenerator"), { ssr: false });
 const Converter = dynamic(() => import ("../components/Converter"), { ssr: false });
@@ -13,15 +14,17 @@ export default function Tools() {
         <>
             <Navbar />
             <br />
+            <main className="flex flex-col items-center bg-gray-100 dark:bg-gray-900 px-6 space-y-8 md:space-y-12">
             <motion.section
                 id="tools"
                 // className="w-full py-20 bg-gray-100 dark:bg-gray-900 text-center transition-all"
-                className="w-full py-20 bg-white dark:bg-gray-900 text-center transition-all"
+                // className="w-full py-20 bg-white dark:bg-gray-900 text-center transition-all"
+                className="py-20 bg-gray-100 dark:bg-gray-900 text-center transition-all"
                 initial={{ opacity: 0, y: 50 }}
                 whileInView={{ opacity: 1, y: 0 }}
                 transition={{ duration: 0.8 }}
             >
-                <h2 className="text-4xl font-bold text-gray-800 dark:text-white">Tools</h2>
+                {/* <h2 className="text-4xl font-bold text-gray-800 dark:text-white">Tools</h2> */}
                 <div className="mt-8 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 max-w-5xl mx-auto">
                     <motion.div className="bg-white dark:bg-gray-800 p-6 rounded-lg shadow-md transition-all">
                         <h3 className="text-xl font-semibold text-gray-700 dark:text-gray-300">Password Generator</h3>
@@ -52,6 +55,8 @@ export default function Tools() {
                     </motion.div>
                 </div>
             </motion.section>
+            <Contact />
+            </main>
         </>
     );
 }
